97ef4c2ee2ea33df75e1c4068785993727bd99c7
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-17 17:16:11 +0500

    17-01-2023

---
+++ 17-01-2023/app.js
@@ -0,0 +1,69 @@
+// let number=+prompt("Enter a number")
+// for(let i=1;i<=10;i++){
+//     document.write(number+"*"+i+"="+number*i+"<br>")
+// }
+
+
+// let number=+prompt("Enter a number")
+// let i=1
+// while(i<=10){
+//     document.write(number+"*"+i+"="+number*i+"<br>")
+//     i++
+// }
+
+
+// let number=+prompt("Enter a number")
+// let i=1
+// do{
+//     document.write(number+"*"+i+"="+number*i+"<br>")
+//     i++
+// }while(i<=10)
+
+
+
+// let inp=prompt("Enter any alphabet")
+// if(inp.toLowerCase()==="a"){
+//     alert("Vowel")
+// }
+// else if(inp.toLowerCase()==="e"){
+//     alert("Vowel")
+// }
+// else if(inp.toLowerCase()==="i"){
+//     alert("Vowel")
+// }
+// else if(inp.toLowerCase()==="o"){
+//     alert("Vowel")
+// }
+// else if(inp.toLowerCase()==="u"){
+//     alert("Vowel")
+// }
+// else{
+//     alert("consonant")
+// }
+
+
+
+// let sentence="I Love My Country"
+// let sentence="I love my country"
+
+// alert(sentence[6])
+// alert(sentence.length)
+// let copy=sentence.slice(2,6)
+// alert(copy)
+// let inp=prompt("Enter what you want to replace")
+// alert(sentence.replaceAll("Cats","Dogs"))
+// alert(sentence.toUpperCase())
+// alert(sentence.toLowerCase())
+// alert(sentence.charAt(sentence.length-1))
+// alert(sentence.lastIndexOf("o"))
+// alert(sentence.charCodeAt(0))
+// let arr=sentence.split(",")
+// alert(sentence.search("like"))
+// alert(sentence.includes("love"))
+// alert(sentence.startsWith("I"))
+
+
+
+
+
+
